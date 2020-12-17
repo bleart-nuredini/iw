04b1b0c98f0f6b93076f6989453be2fdd9281214
bleart nuredini <bleart@gmail.com>
2020-12-17 19:07:22 +0100

    updates

--- js/login-page.js
+++ js/login-page.js
@@ -7,10 +7,10 @@ loginButton.addEventListener("click", (e) => {
     const username = loginForm.username.value;
     const password = loginForm.password.value;
 
-    if (username === "user" && password === "web_dev") {
+    if (username === "user" && password === "123") {
         alert("You have successfully logged in.");
         location.reload();
     } else {
         loginErrorMsg.style.opacity = 1;
     }
-})
+});
--- js/register-page.js
+++ js/register-page.js
@@ -1,4 +1,14 @@
 const registerForm = document.getElementById("register-form");  
 const registerButton = document.getElementById("submit-form");  
+const registerErrorMsg = document.getElementById("register-error-msg");
 
-// implement the event listener. . . 
+registerButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    var username = registerForm.username.value;
+    var password = registerForm.password.value;
+    var email = registerForm.email.value;
+
+    if (username === '' || password === '' || email === '') {
+        registerErrorMsg.style.opacity = 1;
+    }
+});
