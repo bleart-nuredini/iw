1d9c9a05ef0c09d3452cea4245ecb5a80052bc21
bleart nuredini <nuredini.bleart@gmail.com>
2021-02-13 17:11:10 +0100

    header fonts

---
+++ components/fonts.php
@@ -0,0 +1,6 @@
+  <!-- For fonts -->
+  <link src="https://kit.fontawesome.com/a076d05399.js"></link>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+  <link src="https://kit.fontawesome.com/a076d05399.js"></link>
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@600&display=swap" rel="stylesheet">
