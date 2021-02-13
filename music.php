937d8bf0fe435b7f16b97b588361427d5e89a5e3
bleart nuredini <nuredini.bleart@gmail.com>
2021-02-13 17:10:26 +0100

    added a component for fonts

--- music.php
+++ music.php
@@ -4,9 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles/music-style.css"
-    <link src="https://kit.fontawesome.com/a076d05399.js"></link>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-    <link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@600&display=swap" rel="stylesheet">
+    <?php include 'components/fonts.php' ?>
 </head>
 
     <body>
