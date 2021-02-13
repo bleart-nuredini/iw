0057a1f27225750f92e1ce18b091d2126b44a6e0
bleart nuredini <nuredini.bleart@gmail.com>
2021-02-13 12:00:42 +0100

    modev to components

--- components/footer.php
+++ components/footer.php
@@ -1,36 +1,36 @@
-    <footer>
-      <div class="footer-contact">
-        <h1 id="footer-name">Kings of Leon</h1>
-        <h2>Contact</h2>
-        <address>3123-233-32331</address><br>
-        <a class="footer-button" href="mailto:kingsofleon@gmail.com">Email</a>
-      </div>
-      <div class="footer-nav">
-        <ul>
-          <li class="footer-nav-item">
-            <h2>Media</h2>
-            <ul class="footer-ul">
-              <li><a href="https://www.facebook.com/KingsOfLeon/">Facebook</a></li>
-              <li><a href="https://www.instagram.com/kingsofleon/?hl=en">Instagram</a></li>
-              <li><a href="https://twitter.com/kingsofleon?lang=en">Twitter</a></li>
-            </ul>
-          </li>
-          <li class="footer-nav-item">
-            <h2>Concerts</h2>
-            <ul class="footer-ul">
-              <li><a href="#">USA</a></li>
-              <li><a href="#">Europe</a></li>
-              <li><a href="#">Australia</a></li>
-            </ul>
-          </li>
-          <li class="footer-nav-item">
-            <h2>Legal</h2>
-            <ul class="footer-ul">
-              <li><a href="#">Terms of use</a></li>
-              <li><a href="#">Privacy</a></li>
-              <li><a href="#">Sitemap</a></li>
-            </ul>
-          </li>
+<footer>
+  <div class="footer-contact">
+    <h1 id="footer-name">Kings of Leon</h1>
+    <h2>Contact</h2>
+    <address>3123-233-32331</address><br>
+    <a class="footer-button" href="mailto:kingsofleon@gmail.com">Email</a>
+  </div>
+  <div class="footer-nav">
+    <ul>
+      <li class="footer-nav-item">
+        <h2>Media</h2>
+        <ul class="footer-ul">
+          <li><a href="https://www.facebook.com/KingsOfLeon/">Facebook</a></li>
+          <li><a href="https://www.instagram.com/kingsofleon/?hl=en">Instagram</a></li>
+          <li><a href="https://twitter.com/kingsofleon?lang=en">Twitter</a></li>
         </ul>
-      </div>
-    </footer>
+      </li>
+      <li class="footer-nav-item">
+        <h2>Concerts</h2>
+        <ul class="footer-ul">
+          <li><a href="#">USA</a></li>
+          <li><a href="#">Europe</a></li>
+          <li><a href="#">Australia</a></li>
+        </ul>
+      </li>
+      <li class="footer-nav-item">
+        <h2>Legal</h2>
+        <ul class="footer-ul">
+          <li><a href="#">Terms of use</a></li>
+          <li><a href="#">Privacy</a></li>
+          <li><a href="#">Sitemap</a></li>
+        </ul>
+      </li>
+    </ul>
+  </div>
+</footer>
--- components/nav.php
+++ components/nav.php
@@ -1,11 +1,23 @@
-    <!-- Navigation Bar -->
-    <nav>
-      <ul class="nav-bar">
-        <li><a href="index.php">Home</a></li>
-        <li><a href="slider.php">Slider</a></li>
-        <li><a href="about.php">About</a></li>
-        <li><a href="login.php">Login</a></li>
-        <li><a href="register.php">Register</a></li>
-        <li><a href="https://github.com/bleart-nuredini/iw">GitHub</a></li>
-      </ul>
-    </nav>
+<?php
+  session_start();
+  $username = $_SESSION['un'];
+  if (isset($username)) $greeting = "Welcome, " . $username;
+  else $greeting = "";
+?>
+
+<nav>
+  <ul class="nav-bar">
+    <li><a href="index.php">Home</a></li>
+    <li><a href="scroller.php">Scroller</a></li>
+    <li><a href="about.php">About</a></li>
+    <li><a href="login.php">Login</a></li>
+    <li><a href="register.php">Register</a></li>
+    <li><a href="https://github.com/bleart-nuredini/iw">GitHub</a></li>
+    <li><a href="./logout.php">Log out</a></li>
+  </ul>
+  <ul class="nav-side">
+    <li>
+      <span style="color: white;"><?php echo $greeting ?></span>
+    </li>
+  </ul>
+</nav>
