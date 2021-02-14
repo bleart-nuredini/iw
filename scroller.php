8f09c1e33b8aaee68aac72148f244c33681b18de
bleart nuredini <nuredini.bleart@gmail.com>
2021-02-14 06:02:42 +0100

    updated scroller.php

--- scroller.php
+++ scroller.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Kings of Leon - Scroller</title>
+    <title>Scroller</title>
     <link rel="stylesheet" type="text/css" href="styles/slider-style.css">
     <link rel="stylesheet" type="text/css" href="styles/footer.css">
     <link rel="shortcut icon" href="images/favicon.ico">
