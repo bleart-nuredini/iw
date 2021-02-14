d013c457444e9ee3e09152964e979998668328b6
bleart nuredini <nuredini.bleart@gmail.com>
2021-02-14 06:01:44 +0100

    updated login.php

--- login.php
+++ login.php
@@ -1,38 +1,3 @@
-<?php
-  include 'login_data.php';
-
-  $m = 4;   // number of columns
-  $conn = new mysqli($hn, $un, $pw, $db);
-  if ($conn->connect_error) die("Fatal error");
-
-  $res = $conn->query("SELECT * FROM user");
-  if (!$res) die("Could not access the database");
-
-  if (isset($_POST['username']) &&
-      isset($_POST['password']) &&
-      isset($_POST['login'])) { 
-    session_start();
-    $_SESSION['un'] = $_POST['username'];
-    $_SESSION['pw'] = $_POST['username'];
-
-    for ($i = 0; $i < $res->num_rows; $i++) {
-      $row = $res->fetch_array(MYSQLI_NUM);
-
-      if (isInDB($row)) {
-        header("Location:index.php");
-      }
-    }
-  }
-
-  // returns true if the given parameter is in the database
-  function isInDB($row) {
-    if ($row[1] === $_POST['username'] && $row[3] === $_POST['password']) {
-      return true;
-    }
-
-    return false;
-  }
-?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -40,9 +5,8 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="shortcut icon" href="images/favicon.ico">
-  <title>Kings of Leon - Login</title>
+  <title>Login</title>
   <link rel="stylesheet" type="text/css" href="styles/login-style.css">
-  <!-- <script defer src="js/login-page.js"></script> -->
 </head>
 <body>
   <main id="main-container">
@@ -53,11 +17,12 @@
       <p id="login-error-msg">Sorry, you entered an invalid username or passowrd.</p>
     </div>
 
-    <form action="login.php" method="POST" id="login-form">
+    <form action="login_auth.php" method="POST" id="login-form">
       <input type="text" name="username" id="username-fld" class="login-fld" placeholder="username">
       <input type="password" name="password" id="password-fld" class="login-fld" placeholder="password">
       <input type="submit" name="login" value="login" id="form-submit">
     </form>
   </main>
+  <script defer src="js/login-auth.js"></script>
 </body>
 </html>
