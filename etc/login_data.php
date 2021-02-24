20fcc6c5fa01cea36efbe24d192de05c14c020b0
bleart nuredini <nuredini.bleart@gmail.com>
2021-02-24 11:46:33 +0100

    moved login_data.php

---
+++ etc/login_data.php
@@ -0,0 +1,7 @@
+<?php
+  /* Necessary data to establish database connection. */
+  $hn = 'localhost';  // hostname
+  $un = 'demo';       // username
+  $pw = 'demo';       // password
+  $db = 'iw';         // database name
+?>
