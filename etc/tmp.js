edbe95cb4b414a12287ceab7b855daa6183f8a4a
bleart nuredini <nuredini.bleart@gmail.com>
2021-02-24 11:41:19 +0100

    removed etc files

--- etc/tmp.js
+++
@@ -1,7 +0,0 @@
-const goBtn = document.getElementById('go-btn');
-const heyTxt = document.getElementById('hey-text');
-
-goBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    heyTxt.style.opacity = 1;
-});
--- etc/tmp2.php
+++
@@ -1,3 +0,0 @@
-<?php
-    session_start();
-    echo "hello " . $_SESSION["input"];
